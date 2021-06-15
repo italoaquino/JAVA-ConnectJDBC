10948fda3f67dd78266cffbdf39ff2c31b42d791
Italo aquino <italo.aquino.santos@gmail.com>
2021-06-15 01:07:29 -0300

    Segundo

---
+++ src/Application/Program.java
@@ -0,0 +1,14 @@
+package Application;
+
+import Model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department obj = new Department(1, "books");
+		
+		System.out.println(obj);
+
+	}
+
+}
