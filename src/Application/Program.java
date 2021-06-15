bfd3ae7a8bb85f6ffdd4c0e5363339f38a2a7190
Italo aquino <italo.aquino.santos@gmail.com>
2021-06-15 01:27:03 -0300

    Terceiro

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,14 +1,21 @@
 package Application;
 
+import java.sql.Date;
+
 import Model.entities.Department;
+import Model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
 		Department obj = new Department(1, "books");
+
+		Seller seller = new Seller(21, "bob", "boob@gmail.com", new Date(1), 2000.0 , obj);
+
 		
 		System.out.println(obj);
-
+		System.out.println(seller);
 	}
-
+	
 }
